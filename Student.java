c58549632a9cf3e0fa9032c431c0dcfa943d2230
monalisaiembca2026pal <monalisaiembca2026pal@gmail.com>
2025-08-27 15:15:24 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+class Student {
+ int id;
+ String name;
+ int age;
+ Student(int id, String name, int age) {
+ this.id = id;
+ this.name = name;
+ this.age = age;
+ }
+ public String toString() {
+ return "ID: " + id + ", Name: " + name + ", Age: " + age;
+ }
+}
